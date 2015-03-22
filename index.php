88acc0711b4263017860332e29ad32fa7a7549df
bstrx <bstrxx@gmail.com>
2015-03-22 16:31:17 -0400

    initial markup

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+</head>
+<body>
+    <div class="header">
+        <div class="small"></div>
+    </div>
+    <div class="general">
+        <div class="small"></div>
+    </div>
+    <div class="service">
+        <div class="small"></div>
+    </div>
+    <div class="music">
+        <div class="small"></div>
+    </div>
+    <div class="experience">
+        <div class="small"></div>
+    </div>
+    <div class="contacts">
+        <div class="small">
+            <form>
+                <input type="text" name="name"/>
+                <input type="text" name="contacts"/>
+                <input type="text" name="description"/>
+                <input type="submit" value="Отправить"/>
+            </form>
+
+        </div>
+    </div>
+</body>
+</html>
+
+<?php
+
