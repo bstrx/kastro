55f7f197c5504411019638554b2e589e606faef6
Vladimir Prudilin <bstrxx@gmail.com>
2015-03-26 20:41:49 +0300

    Rich background example

--- index.php
+++ index.php
@@ -1,26 +1,72 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="main.css"/>
 </head>
 <body>
-    <div class="header">
-        <div class="small"></div>
+    <div id="header">
+        <div class="small">
+            Там, где женщина играет роль первой скрипки, дирижеры ни к чему.
+        </div>
     </div>
-    <div class="general">
-        <div class="small"></div>
+    <div id="general">
+        <div class="small">
+            <img src="image/violin.jpg" id='violin'>
+            <div style="margin-left: 210px">
+                <h1>Живое исполнение на скрипке</h1>
+                <br>
+                <p>Каждый веб-разработчик знает, что такое текст-«рыба». Текст этот, несмотря на название, не имеет никакого отношения к обитателям водоемов.
+                    Используется он веб-дизайнерами для вставки на интернет-страницы и демонстрации внешнего вида контента, просмотра шрифтов, абзацев, отступов и т.д. Так как цель применения такого текста исключительно демонстрационная, то и смысловую нагрузку ему нести совсем необязательно. Более того, нечитабельность текста сыграет на руку при оценке качества восприятия макета.</p>
+            </div>
+        </div>
     </div>
-    <div class="service">
-        <div class="small"></div>
+    <div id="service">
+        <div class="small">
+            <div class="service-item">
+                <img src="image/quavers.png">
+                <div class="item-head">
+                    Выступление на праздниках
+                </div>
+                <div class="item-body">
+                    Живая скрипка скрасит любой корпоратив или банкет
+                </div>
+            </div>
+            <div class="service-item">
+                <img src="image/quavers.png">
+                <div class="item-head">
+                    Музыка для танца молодожёнов
+                </div>
+                <div class="item-body">
+                    Для такого романтичного момента
+                </div>
+            </div>
+            <div class="service-item">
+                <img src="image/quavers.png">
+                <div class="item-head">
+                    Музыкальный фон для свидания
+                </div>
+                <div class="item-body">
+                    Живая музыка поможет сделать свидание особенным
+                </div>
+            </div>
+            <div class="service-item">
+                <img src="image/quavers.png">
+                <div class="item-head">
+                    Индивидуальный подбор музыки
+                </div>
+                <div class="item-body">
+                    Вместе с вами мы можем подобрать ту музыку, которую вы предпочитаете
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="music">
-        <div class="small"></div>
+    <div id="experience">
+        <div class="small">
+            <h2>Опыт выступлений:</h2>
+        </div>
     </div>
-    <div class="experience">
-        <div class="small"></div>
-    </div>
-    <div class="contacts">
+    <div id="contacts">
         <div class="small">
             <form>
                 <input type="text" name="name"/>
@@ -28,7 +74,6 @@
                 <input type="text" name="description"/>
                 <input type="submit" value="Отправить"/>
             </form>
-
         </div>
     </div>
 </body>
